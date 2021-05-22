d4f4e67f562a386ff7acf06c6e521d76bc9c3150
0x1A4 <yannickmonjeamb@icloud.com>
2021-05-22 09:24:28 +0200

    added correct production backend url

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const BACKEND_HOST = (process.env.NODE_ENV === 'production' ? 'https://betgame-server.herokuapp.org' : 'http://localhost')
+export const BACKEND_HOST = (process.env.NODE_ENV === 'production' ? 'https://betgame-server.herokuapp.com' : 'http://localhost')
 export const BACKEND_PORT = (process.env.NODE_ENV === 'production' ? 80 : 4000)
